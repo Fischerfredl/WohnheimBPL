647dac79ce1bde04b232715b422fbdf8266bc193
Fischerfredl <alfred.melch@gmx.de>
2016-04-06 16:37:28 +0200

    Update config_linux.py

--- config/config_linux.py
+++ config/config_linux.py
@@ -1,6 +1,8 @@
 DATABASE = 'database.db'
 DEBUG = True
-SECRET_KEY = 'set one'
+SECRET_KEY = '=R(Db4%ijH3GZd)1cmirMy7sP(%V95=h'
+IPADDR = '0.0.0.0'
+PORT = 5000
 
 # acc for administrator
 ADMINLOGIN = 'admin'
@@ -8,4 +10,4 @@ ADMINPASSWORD = 'password'
 
 # acc for tournament mods
 MODLOGIN = 'mod'
-MODPASSWORD = 'mod'
+MODPASSWORD = 'password'
