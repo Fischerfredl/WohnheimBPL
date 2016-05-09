b6b5b3fc75fdda78bec919f6b1f3aef18e9f2c0e
Fischerfredl <alfred.melch@gmx.de>
2016-05-09 10:53:40 +0200

    'edit game' kind of  implemented

--- settings/functions.py
+++ settings/functions.py
@@ -1,6 +1,7 @@
 from flask import request, flash, current_app, session
 from functions_general import query_db, update_db, get_last_id, verify_password, set_password, set_new_password, \
     get_users
+from competition.functions import get_league_game_result
 
 
 def get_form(option):
@@ -39,7 +40,25 @@ def get_form(option):
     elif option == 'player_assign_team':
         pass
     elif option == 'game_edit':
-        pass
+        if not session['gameid']:
+            form_input = [('Kein Spiel gewaehlt', 'label', 'label', '')]
+        elif session['username'] != 'mod' and query_db('SELECT SiegerID FROM Spiel WHERE SpielID = ?', [session['gameid']], one=True):
+            form_input = [('Spiel bereits eingetragen', 'label', 'label', '')]
+        else:
+            result = get_league_game_result(session['gameid'])
+            var = [0, 1, 2, 3, 4, 5, 6]
+            divisionid = query_db('SELECT UnterwbID FROM Spiel WHERE SpielID = ?', [result[0]], one=True)
+            form_input = [('SpielID: %i'%result[0], 'label', 'label', ''),
+                          (result[2]+' treffer', 'select_list', 't1tref', var),
+                          ('Spieler1', 'select_list', 'sp1', get_players(result[2], divisionid)),
+                          ('Spieler1 treffer', 'select_list', 'sp1tref', var),
+                          ('Spieler2', 'select_list', 'sp2', get_players(result[2], divisionid)),
+                          ('Spieler2 treffer', 'select_list', 'sp2tref', var),
+                          (result[5]+' treffer', 'select_list', 't2tref', var),
+                          ('Spieler3', 'select_list', 'sp3', get_players(result[5], divisionid)),
+                          ('Spieler3 treffer', 'select_list', 'sp3tref', var),
+                          ('Spieler4', 'select_list', 'sp4', get_players(result[5], divisionid)),
+                          ('Spieler4 treffer', 'select_list', 'sp4tref', var)]
     return [{'label': row[0], 'type': row[1], 'name': row[2], 'value': row[3]} for row in form_input]
 
 
@@ -178,7 +197,58 @@ def competition_delete():
     return
 def player_assign_team():
     return
+
+
 def game_edit():
-    return
+    if not session['gameid']:
+        flash('error: no game')
+        return False
+    gameid = session['gameid']
+    t1tref = int(request.form['t1tref'])
+    t2tref = int(request.form['t2tref'])
+    sp1 = request.form['sp1']
+    sp2 = request.form['sp2']
+    sp3 = request.form['sp3']
+    sp4 = request.form['sp4']
+    sp1id = query_db('SELECT SpielerID FROM Spieler WHERE Nickname = ?', [sp1], one=True)
+    sp2id = query_db('SELECT SpielerID FROM Spieler WHERE Nickname = ?', [sp2], one=True)
+    sp3id = query_db('SELECT SpielerID FROM Spieler WHERE Nickname = ?', [sp3], one=True)
+    sp4id = query_db('SELECT SpielerID FROM Spieler WHERE Nickname = ?', [sp4], one=True)
+    sp1tref = int(request.form['sp1tref'])
+    sp2tref = int(request.form['sp2tref'])
+    sp3tref = int(request.form['sp3tref'])
+    sp4tref = int(request.form['sp4tref'])
+    if t1tref == 6 and t2tref == 6 or t1tref != 6 and t2tref != 6:
+        flash('error: no winning team')
+        return False
+    if sp1tref+sp2tref > t1tref:
+        flash('error: team 1 players to much cups')
+        return False
+    if sp3tref+sp4tref > t2tref:
+        flash('error: team 2 players too much cups')
+        return False
+    if sp1 == sp2:
+        flash('error: identical players in team 1')
+        return False
+    if sp3 == sp4:
+        flash('error: identical players in team 2')
+        return False
+    if t1tref == 6:
+        winner = query_db('SELECT Team1ID FROM Spiel WHERE SpielID = ?', [gameid], one=True)
+    else:
+        winner = query_db('SELECT Team2ID FROM Spiel WHERE SpielID = ?', [gameid], one=True)
+    update_db('UPDATE Spiel SET SiegerID = ? WHERE SpielID = ?', [winner, gameid])
+    update_db('UPDATE Ligaspiel SET T1Tref = ? WHERE SpielID = ?', [t1tref, gameid])
+    update_db('UPDATE Ligaspiel SET T2Tref = ? WHERE SpielID = ?', [t2tref, gameid])
+    update_db('DELETE FROM Ligaspieler WHERE SpielID = ?', [gameid])
+    update_db('INSERT INTO Ligaspieler(SpielID, SpielerID, Treffer) VALUES (?, ?, ?)', [gameid, sp1id, sp1tref])
+    update_db('INSERT INTO Ligaspieler(SpielID, SpielerID, Treffer) VALUES (?, ?, ?)', [gameid, sp2id, sp2tref])
+    update_db('INSERT INTO Ligaspieler(SpielID, SpielerID, Treffer) VALUES (?, ?, ?)', [gameid, sp3id, sp3tref])
+    update_db('INSERT INTO Ligaspieler(SpielID, SpielerID, Treffer) VALUES (?, ?, ?)', [gameid, sp4id, sp4tref])
+    flash('Spiel eingetragen')
+    return True
 
 
+def get_players(teamname, divisionid):
+    return [row[0] for row in query_db('SELECT (SELECT Nickname FROM Spieler WHERE Spieler.SpielerID = Teilgenommen.SpielerID) \
+              FROM Teilgenommen WHERE TeamID = (SELECT TeamID FROM Team WHERE Name = ?) AND UnterwbID = ?', [teamname, divisionid])]
