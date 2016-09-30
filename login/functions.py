c5ce0f455f35f6c69b4790aeec732d2da5237231
Fischerfredl <alfred.melch@gmx.de>
2016-09-30 15:18:17 +0200

    Hotfix - MODLOGIN

--- login/functions.py
+++ login/functions.py
@@ -7,7 +7,7 @@ def validate_password(nickname, password):
     fetchpass = query_db('SELECT Passwort FROM Spieler WHERE Nickname = ?', [nickname], one=True)
     if sha1(password+current_app.config['SALT']).hexdigest() == fetchpass or \
             (nickname == current_app.config['ADMINLOGIN'] and password == current_app.config['ADMINPASSWORD']) or \
-            (nickname == current_app.config['MODLOGIN'] and password == current_app.config['ADMINPASSWORD']):
+            (nickname == current_app.config['MODLOGIN'] and password == current_app.config['MODPASSWORD']):
         return True
     else:
         return False
